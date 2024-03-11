a85d54aff439e0999cf566f00abc248e67bb9d48
Saheed <156262825+Saheedadeitan@users.noreply.github.com>
2024-03-10 21:53:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pmdarima as pm
-
-st.title('Auto-ARIMA Forecasting')
+import joblib
 
 uploaded_file = st.file_uploader("Upload CSV file", type=['csv'])
 
@@ -13,12 +11,17 @@ else:
     st.write("Upload a CSV file.")
     st.stop()
 
-# model = pm.auto_arima(data.Sales, seasonal=True, m=12)
-model_Auto = pm.auto_arima(data.Sales, start_p=0, start_q=0, max_p=3, max_q=3, m=12,max_P=2,d=1,
+
+model = pm.auto_arima(data.Sales, start_p=0, start_q=0, max_p=3, max_q=3, m=12,max_P=2,d=1,
     max_D=1,max_Q=2,start_P=0, seasonal=True, trace=True,
                       error_action='ignore', 
                       suppress_warnings=True, 
                       stepwise=True) 
-forecast = model.predict(n_periods=12)  # Forecast the next 12 periods (adjust as needed)
 
+st.title('Auto-ARIMA Forecasting')
+
+forecast = model.predict(n_periods=12)
 st.line_chart(forecast)
+
+  # Display the prediction
+st.write(f"Estimated Price: ${forecast[0]:,.2f}")
